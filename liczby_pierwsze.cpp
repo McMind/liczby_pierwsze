2fb68fb324bef6f2a887eaca44ae81ed7af704f0
McMind <McMind@GIGAMCMIND>
2022-10-17 13:34:47 +0200

    Add project files.

---
+++ liczby_pierwsze.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int liczba = 0;
+	bool zlozony = false;
+	cout << "Wpisz liczbe\n";
+	while (liczba < 2) {
+		cin >> liczba;
+	}
+	for (unsigned int i = liczba/2; i > 1; i--) {
+		if (liczba % i == 0) {
+			zlozony = true;
+			break;
+		}
+	}
+
+	if (!zlozony) {
+		cout << "Liczba jest pierwsza";
+	}
+	else {
+		cout << "Liczba jest zlozona";
+	}
+}
